9c1fb442278ca694096db69bbb555446d2a41596
seece <pekka.vaananen@iki.fi>
2013-10-12 23:26:50 +0300

    spinning octagon

--- GIGAengine/nostatusEffect.cpp
+++ GIGAengine/nostatusEffect.cpp
@@ -13,8 +13,26 @@ NostatusEffect::~NostatusEffect()
 
 }
 
+#define M_PI (3.141592653589)
+
 void NostatusEffect::render(ParameterMap& param)
 {
+
+	int slices = 8;
+
 	fill.use();
-	simple::tri(-1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f + sin(param["t"]));
+
+	float add = param["t"] * 0.5;
+
+	for (int i=0;i<slices;i++)  {
+		float rad = (2.0*M_PI)/float(slices);
+
+		simple::tri(
+			0.0f, 0.0f, 
+			0.0f + cos(add + rad*i), 0.0f + sin(add +rad*i), 
+			0.0f + cos(add + rad*(i+1)), 0.0f + sin(add + rad*(i+1))
+			);
+	}
 }
+
+#undef M_PI
