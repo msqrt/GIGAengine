c574d6a06f45e32bc299cc9d14e2e1c6d6009cda
powly k <pauli.kemppinen@hotmail.com>
2013-10-17 16:26:57 +0300

    displacement test

--- GIGAengine/assets/wall.shader
+++ GIGAengine/assets/wall.shader
@@ -12,15 +12,21 @@ vec3 hsltorgb(vec3 hsl) {
 #ifdef vertexcompile
 
 layout(location=0) in vec3 apos;
+layout(location=1) in vec3 altan;
+layout(location=2) in vec3 artan;
 
 smooth out vec3 nor, dir;
 smooth out vec4 pos;
 
 uniform mat4 projection, camera;
+uniform float t;
 
 void main() {
-	pos = camera*vec4(apos*10000.0, 1.0);
-	nor = vec4(camera*vec4(apos-vec3(.0,.0,1.0),.0)).xyz;
+	vec3 loc = apos*10000.0+sin(vec3(t)+apos*1000.0)*5.0;
+	vec3 loc2 = altan*10000.0+sin(vec3(t)+altan*1000.0)*5.0;
+	vec3 loc3 = artan*10000.0+sin(vec3(t)+artan*1000.0)*5.0;
+	pos = camera*vec4(loc,1.0);
+	nor = vec4(camera*vec4(-normalize(cross(loc2-loc,loc3-loc)),.0)).xyz;
 	dir = reflect(pos.xyz,nor);
 	gl_Position = projection*pos;
 }
@@ -38,8 +44,8 @@ smooth in vec4 pos;
 uniform vec3 primaryDirection, primaryColour, secondaryDirection, secondaryColour;
 
 void main() {
-	float fres = clamp(.01+.3*pow(1.0-dot(normalize(pos.xyz),-normalize(nor)),5.0),.0,1.0);
-	outcol = mix(vec4(.03), vec4((dot(normalize(dir),normalize(primaryDirection))*.5+.5)*hsltorgb(primaryColour) + (dot(normalize(dir),normalize(secondaryDirection))*.5+.5)*hsltorgb(secondaryColour), 1.0), fres);
+	float fres = clamp(.0+.2*pow(1.0-dot(normalize(pos.xyz),-normalize(nor)),15.0),.0,1.0);
+	outcol = mix(vec4(.01), vec4((dot(normalize(dir),normalize(primaryDirection))*.5+.5)*hsltorgb(primaryColour) + (dot(normalize(dir),normalize(secondaryDirection))*.5+.5)*hsltorgb(secondaryColour), 1.0), fres);
 	outadd = vec4(.0);
 }
 
--- GIGAengine/wall.cpp
+++ GIGAengine/wall.cpp
@@ -6,22 +6,34 @@
 #define RES 512
 
 Wall::Wall() : Effect(), s("assets/wall.shader") {
-	float * geom = new float[RES*RES*7];
+	float * geom = new float[RES*RES*13];
 	for(int x = 0; x<RES; x++)
 	for(int y = 0; y<RES; y++) {
 		float dx = int(x-float(RES-1)/2.0f)/float(RES-2);
 		float dy = int(y-float(RES-1)/2.0f)/float(RES-2);
 		float l = 1.0f/sqrt(dx*dx+dy*dy+1.0f);
-		geom[(x*RES+y)*7+0] = dx*l;
-		geom[(x*RES+y)*7+1] = dy*l;
-		geom[(x*RES+y)*7+2] = 1.0f-l;
-		geom[(x*RES+y)*7+3] = rand()/float(RAND_MAX);
-		geom[(x*RES+y)*7+4] = rand()/float(RAND_MAX);
-		geom[(x*RES+y)*7+5] = rand()/float(RAND_MAX);
-		geom[(x*RES+y)*7+6] = rand()/float(RAND_MAX);
+		geom[(x*RES+y)*13+0] = dx*l;
+		geom[(x*RES+y)*13+1] = dy*l;
+		geom[(x*RES+y)*13+2] = 1.0f-l;
+		dx = int(x-1-float(RES-1)/2.0f)/float(RES-2);
+		dy = int(y-float(RES-1)/2.0f)/float(RES-2);
+		l = 1.0f/sqrt(dx*dx+dy*dy+1.0f);
+		geom[(x*RES+y)*13+3] = dx*l;
+		geom[(x*RES+y)*13+4] = dy*l;
+		geom[(x*RES+y)*13+5] = 1.0f-l;
+		dx = int(x-float(RES-1)/2.0f)/float(RES-2);
+		dy = int(y-1-float(RES-1)/2.0f)/float(RES-2);
+		l = 1.0f/sqrt(dx*dx+dy*dy+1.0f);
+		geom[(x*RES+y)*13+6] = dx*l;
+		geom[(x*RES+y)*13+7] = dy*l;
+		geom[(x*RES+y)*13+8] = 1.0f-l;
+		geom[(x*RES+y)*13+9] = rand()/float(RAND_MAX);
+		geom[(x*RES+y)*13+10] = rand()/float(RAND_MAX);
+		geom[(x*RES+y)*13+11] = rand()/float(RAND_MAX);
+		geom[(x*RES+y)*13+12] = rand()/float(RAND_MAX);
 	}
 
-	m.data(RES*RES, 7*sizeof(float), geom);
+	m.data(RES*RES, 13*sizeof(float), geom);
 	delete [] geom;
 
 	unsigned * inds = new unsigned[(RES-1)*(RES-1)*6];
@@ -39,7 +51,9 @@ Wall::Wall() : Effect(), s("assets/wall.shader") {
 	delete [] inds;
 
 	m.pointer(0, 0, 3);
-	m.pointer(1, 3, 4);
+	m.pointer(1, 3, 3);
+	m.pointer(2, 6, 3);
+	m.pointer(3, 9, 4);
 
 }
 
@@ -53,6 +67,7 @@ void Wall::render(ParameterMap& param) {
 	glUniform3f(s.getLoc("primaryColour"), param["primh"], param["prims"], param["primv"]);
 	glUniform3f(s.getLoc("secondaryDirection"), param["secx"], param["secy"], param["secz"]);
 	glUniform3f(s.getLoc("secondaryColour"), param["sech"], param["secs"], param["secv"]);
+	glUniform1f(s.getLoc("t"), param["t"]);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	m.draw(GL_TRIANGLES);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
